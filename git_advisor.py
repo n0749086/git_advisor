cc244dfe01c9bcb3112c80554c015461cac5c458
isiz <n0749086@gmail.com>
2025-01-02 11:56:39 +0900

    add argument parser

--- git_advisor.py
+++ git_advisor.py
@@ -1,8 +1,17 @@
+from argparse import ArgumentParser
 import setting
+
+def parse_arguments():
+    """Parse command-line args"""
+    parser = ArgumentParser(description='What to ask the LLM')
+    parser.add_argument('--review', '-r', action='store_true', help='code review')
+    parser.add_argument('--commit', '-c', action='store_true', help='git commit message')
+    return parser.parse_args()
 
 def main():
 	print("API_KEY:", setting.API_KEY)
 	print("USE_GPT:", setting.USE_GPT)
+	print(parse_arguments())
 
 if __name__ == '__main__':
 	main()
