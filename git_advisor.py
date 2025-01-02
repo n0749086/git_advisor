7a6138dbd559b6f35a2f495ca96003cf5fa02b96
isiz <n0749086@gmail.com>
2025-01-02 11:57:10 +0900

    add get diff function

--- git_advisor.py
+++ git_advisor.py
@@ -1,4 +1,6 @@
 from argparse import ArgumentParser
+import subprocess
+
 import setting
 
 def parse_arguments():
@@ -8,10 +10,16 @@ def parse_arguments():
     parser.add_argument('--commit', '-c', action='store_true', help='git commit message')
     return parser.parse_args()
 
+def get_git_diff():
+    """output git diff HEAD"""
+    result = subprocess.run(['git', 'diff', 'HEAD~3'], stdout=subprocess.PIPE)
+    return result.stdout.decode('utf-8')
+
 def main():
 	print("API_KEY:", setting.API_KEY)
 	print("USE_GPT:", setting.USE_GPT)
 	print(parse_arguments())
+	print(get_git_diff())
 
 if __name__ == '__main__':
 	main()
